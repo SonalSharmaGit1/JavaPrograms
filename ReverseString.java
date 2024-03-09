3c9a41c270682ae9efa98e4cf775cf5505176a24
SonalSharmaGit1 <sonalssharma06@gmail.com>
2024-03-09 15:26:07 +0530

    First commit

---
+++ ReverseString.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+import org.testng.annotations.Test;
+
+public class ReverseString {
+
+	@Test
+	public void reverseString()
+	{
+		String str="modi is my god";
+		String rev=" ";
+		char k;
+		System.out.println("Original String :"+" "+str);
+		for(int i=0;i<str.length();i++)
+		{
+			k=str.charAt(i);
+			rev=k+rev;
+		}
+		System.out.println("Reversed String :"+" "+rev);
+		}
+}
